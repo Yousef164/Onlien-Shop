447ace6fe609137daa9d331dce98fa2b9351a452
Yousef Salah <ys1222006@gmail.com>
2025-07-17 03:53:23 +0300

    ..

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,10 +1,13 @@
 const authModel = require('../models/auth.model');
+const validationResult = require('express-validator').validationResult
+
 
 exports.getSignup = (req, res, next) => {
     res.render('signup', { title: 'Sign Up' });
 }
 
 exports.postSignup = (req, res, next) => {
+    return console.log(validationResult(req).array());
     authModel.createNewUser(req.body.username, req.body.email, req.body.password)
     .then(()=> res.redirect('/login'))
     .catch(err => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
+const check = require('express-validator').check;
 
 const authController = require('../controller/auth.controller');
 
 router.get('/signup', authController.getSignup);
 
-router.post('/signup', bodyParser.urlencoded({extended: true}), authController.postSignup);
+router.post('/signup',
+    bodyParser.urlencoded({extended: true}),
+    check('username').notEmpty(),
+    check('email').notEmpty().isEmail(),
+    check('password').isLength({ min: 6}),
+    check('confirmPassword').custom((value, { req }) => {
+        if(value !== req.body.password) {
+            throw 'Passwords do not match';
+        }
+        else return true;
+    }),
+    authController.postSignup
+);
 
 router.get('/login', authController.getLogin);
 
